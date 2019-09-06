20adc80d731b54ef8c57db20637c1c9bee064282
picoterama <54046389+picoterama@users.noreply.github.com>
2019-09-06 18:21:20 -0400

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://www.googleapis.com/drive/v3/files/1wAn9kmyzxnyOEAvgcS5eW3MBT8IYosmi?alt=media&key=AIzaSyCe1yD7WPomvXq6kbtbQBRD_z5_VA-4Ho0'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['picasso', 'monet', 'vincent']
 path = Path(__file__).parent
 
 app = Starlette()
